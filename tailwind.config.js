d6ddae875afdda9bcf3aa6a1d17299763ba44cc7
Lucas Cerri <lucascerri3@outlook.de>
2023-10-28 18:58:36 +0200

    initial release

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./public/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile': '375px',
+      },
+      fontFamily: {
+        kumbh_sans: ["Kumbh Sans", "sans-serif"]
+      },
+      colors: {
+        // primary
+        dark_cyan: "hsl(185, 75%, 39%)",
+        very_dark_desaturated: "hsl(229, 23%, 23%)",
+        dark_grayish_blue: "hsl(227, 10%, 46%)",
+        // neutral
+        dark_gray: "hsl(0, 0%, 59%)"
+      }
+    },
   },
   plugins: [],
 }
